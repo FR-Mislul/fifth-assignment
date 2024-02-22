d26563118482b0a94775c8cc082b57d6a5bd1f7c
FR Mislul <frmislul421@gmail.com>
2024-02-22 23:57:49 +0600

    resubmit

--- js/scripet.js
+++ js/scripet.js
@@ -1,11 +1,28 @@
-// scrool drop down 
-function scrollDopDown(){
-    document.getElementById('sell-ticket').scrollIntoView('sell-ticket')
-}
+const allSit = document.getElementsByClassName('bus-sit')
 
-// sit sec
+for (const sit of allSit){
+    sit.addEventListener('click', function(event){
+        const firsSit = event.target.innerText;
+        const selectedTicket = document.getElementById('tickit-1');
+        const div = document.createElement('div')
+        div.classList.add('sit-style')
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        p1.innerText=firsSit;
+        p2.innerText='Economoy';
+        p3.innerText='550';
+        div.append(p1);
+        div.append(p2);
+        div.append(p3);
+        selectedTicket.append(div)
 
-const allItems = document.getElementsByClassName('items');
+        const singleTicket = p3.innerText;
+        updateTotalCost (singleTicket);
+        updateGrandTotal();
+    })}
+
+    const allItems = document.getElementsByClassName('items');
 
 let sitCount = 0;
 let availableSit = 8;
@@ -59,3 +76,30 @@ document.getElementById('b3').addEventListener('click', function(){
 document.getElementById('b4').addEventListener('click', function(){
     this.style.backgroundColor='#1DD100'
 })
+
+    function updateGrandTotal(stutas){
+        if (stutas== undefined){
+            const totalPrice = getConvertSitValue('bay-of-cost');
+        document.getElementById('grand-total').innerText = totalPrice;
+        }
+        const cuponCode = document.getElementById('cupon-code').value;
+        if(cuponCode=='NEW15'|| 'Couple 20'){
+            const discound = totalPrice * .2;
+        }
+        else{
+            alert('please enter a valid cupone code')
+        }
+    }
+
+    function updateTotalCost (value){
+        const totalPrice = getConvertSitValue('bay-of-cost')
+        const sum = totalPrice + parseInt(value) ;
+        document.getElementById('bay-of-cost').innerText = sum;
+    }
+
+    function getConvertSitValue(id){
+        // const sitOfBuss = document.getElementById(id).style.backgroundColor='#1DD100';
+        const sitPrice= document.getElementById(id).innerText;
+        const sitPriceTK = parseInt(sitPrice);
+        return sitPriceTK;
+    }
